9a66a0ff322fe243e85883236317d2321ba15734
Cλstor <ygorcastor@gmail.com>
2026-03-25 09:30:00 +0100

    chore: drop default mailer and page scaffolding

--- lib/gingko/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Gingko.Mailer do
-  use Swoosh.Mailer, otp_app: :gingko
-end
--- lib/gingko_web/controllers/page_controller.ex
+++ lib/gingko_web/controllers/page_controller.ex
@@ -2,6 +2,18 @@ defmodule GingkoWeb.PageController do
   use GingkoWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    settings = Gingko.Settings.load(settings_opts())
+
+    if settings.ready? do
+      redirect(conn, to: ~p"/projects")
+    else
+      conn
+      |> put_flash(:info, "Setup required before Gingko can use your configured providers.")
+      |> redirect(to: ~p"/setup")
+    end
+  end
+
+  defp settings_opts do
+    Application.get_env(:gingko, :settings_opts, [])
   end
 end
